3a1dbb282edeb7f9118842576f663d52d17d4b65
Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
2009-05-26 15:21:43 +0300

    Don't check for vala unless instructed to do so
    With this change people building from distribution tarball will not
    (hopefully) need vala. Yay!

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,4 @@
 mkdir -p m4
 autoreconf -v --install || exit 1
 glib-gettextize --force --copy || exit 1
-./configure --enable-maintainer-mode --enable-debug "$@"
+./configure --enable-vala --enable-maintainer-mode --enable-debug "$@"
