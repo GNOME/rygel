73decacee355bb35ae26df82cd54b5a47ad6654e
Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
2009-06-04 16:19:47 +0300

    core: autogen.sh enables build for all plugins

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,5 @@
 mkdir -p m4
 autoreconf -v --install || exit 1
 glib-gettextize --force --copy || exit 1
-./configure --enable-vala --enable-maintainer-mode --enable-debug "$@"
+./configure --enable-vala --enable-maintainer-mode --enable-debug \
+            --enable-test-plugin --enable-mediathek-plugin "$@"
