GNOME/rygel

commit c398c260e798095496f735ebf32c6549baef928b

core,plugins: Port to vala 0.11.1

src/rygel/rygel-seekable-response.vala
@@ -31,7 +31,7 @@ internal class Rygel.SeekableResponse : Rygel.HTTPResponse {
     private File file;
     private FileInputStream input_stream;
 
-    private char[] buffer;
+    private uint8[] buffer;
     private size_t total_length;
 
     int priority;
@@ -50,7 +50,7 @@ internal class Rygel.SeekableResponse : Rygel.HTTPResponse {
         this.priority = this.get_requested_priority ();
         this.total_length = (size_t) seek.length;
 
-        this.buffer = new char[SeekableResponse.BUFFER_LENGTH];
+        this.buffer = new uint8[SeekableResponse.BUFFER_LENGTH];
         this.file = File.new_for_uri (uri);
     }
 
@@ -108,16 +108,10 @@ internal class Rygel.SeekableResponse : Rygel.HTTPResponse {
         yield this.close_stream ();
     }
 
-    private size_t bytes_to_read () {
-        return size_t.min (this.total_length, SeekableResponse.BUFFER_LENGTH);
-    }
-
     private async void read_contents () throws Error {
-        var bytes_read = yield this.input_stream.read_async (
-                                        this.buffer,
-                                        this.bytes_to_read (),
-                                        this.priority,
-                                        this.cancellable);
+        var bytes_read = yield this.input_stream.read_async (this.buffer,
+                                                             this.priority,
+                                                             this.cancellable);
         this.msg.wrote_chunk.connect ((msg) => {
             if (this.run_continue != null) {
                 this.run_continue ();
@@ -138,11 +132,9 @@ internal class Rygel.SeekableResponse : Rygel.HTTPResponse {
                 break;
             }
 
-            bytes_read = yield this.input_stream.read_async (
-                                        this.buffer,
-                                        this.bytes_to_read (),
-                                        this.priority,
-                                        this.cancellable);
+            bytes_read = yield this.input_stream.read_async (this.buffer,
+                                                             this.priority,
+                                                             this.cancellable);
         }
     }
 

src/plugins/media-export/rygel-media-export-harvesting-task.vala
@@ -332,7 +332,7 @@ public class Rygel.MediaExport.HarvestingTask : Rygel.StateMachine, GLib.Object 
                                         FileQueryInfoFlags.NONE,
                                         this.cancellable);
             string content_type = info.get_content_type ();
-            string mime = g_content_type_get_mime_type (content_type);
+            string mime = ContentType.get_mime_type (content_type);
             uint64 size = info.get_size ();
             uint64 mtime = info.get_attribute_uint64 (
                                         FILE_ATTRIBUTE_TIME_MODIFIED);

src/plugins/media-export/rygel-media-export-metadata-extractor.vala
@@ -165,7 +165,7 @@ public class Rygel.MediaExport.MetadataExtractor: GLib.Object {
         }
 
         string content_type = file_info.get_content_type ();
-        mime = g_content_type_get_mime_type (content_type);
+        mime = ContentType.get_mime_type (content_type);
         size = file_info.get_size ();
         mtime = file_info.get_attribute_uint64 (FILE_ATTRIBUTE_TIME_MODIFIED);
     }

src/rygel/rygel-gst-utils.vala
@@ -120,10 +120,8 @@ internal abstract class Rygel.GstUtils {
         return factory.get_klass ().contains ("Depayloader");
     }
 
-    private static int compare_factories (void *a, void *b) {
-        ElementFactory factory_a = (ElementFactory) a;
-        ElementFactory factory_b = (ElementFactory) b;
-
+    private static int compare_factories (ElementFactory factory_a,
+                                          ElementFactory factory_b) {
         return (int) (factory_b.get_rank () - factory_a.get_rank ());
     }
 }

src/rygel/rygel-http-post.vala
@@ -84,7 +84,7 @@ internal class Rygel.HTTPPost : HTTPRequest {
 
     private async void write_chunk (Buffer chunk) {
         try {
-            this.stream.write (chunk.data, chunk.length, this.cancellable);
+            this.stream.write (chunk.data.data, this.cancellable);
         } catch (Error error) {
             this.handle_error (error);
             this.handle_continue ();

src/rygel/rygel-http-response.vala
@@ -58,10 +58,8 @@ internal abstract class Rygel.HTTPResponse : GLib.Object, Rygel.StateMachine {
         this.end (true, Soup.KnownStatusCode.CANCELLED);
     }
 
-    public void push_data (void *data, size_t length) {
-        this.msg.response_body.append (Soup.MemoryUse.COPY,
-                                       data,
-                                       length);
+    public void push_data (uint8[] data, size_t length) {
+        this.msg.response_body.append (Soup.MemoryUse.COPY, data);
 
         this.server.unpause_message (this.msg);
     }

src/rygel/rygel-media-item.vala
@@ -107,10 +107,8 @@ public abstract class Rygel.MediaItem : MediaObject {
         this.uris.add (uri);
     }
 
-    internal int compare_transcoders (void *a, void *b) {
-        var transcoder1 = (Transcoder) a;
-        var transcoder2 = (Transcoder) b;
-
+    internal int compare_transcoders (Transcoder transcoder1,
+                                      Transcoder transcoder2) {
         return (int) transcoder1.get_distance (this) -
                (int) transcoder2.get_distance (this);
     }

src/rygel/rygel-plugin-loader.vala
@@ -120,7 +120,7 @@ public class Rygel.PluginLoader : Object {
             File file = File.new_for_path (file_path);
             FileType file_type = info.get_file_type ();
             string content_type = info.get_content_type ();
-            string mime = g_content_type_get_mime_type (content_type);
+            string mime = ContentType.get_mime_type (content_type);
 
             if (file_type == FileType.DIRECTORY) {
                 // Recurse into directories

commit 4b14f8b1b8ef3f313bb72b1dbf5cb06a76fdebcf

core: Only push read bytes, not the whole buffer
This corrects a serious regression in seekable HTTP GET handling
introduced by commit c398c26.

src/rygel/rygel-seekable-response.vala
@@ -119,7 +119,10 @@ internal class Rygel.SeekableResponse : Rygel.HTTPResponse {
         });
 
         while (bytes_read > 0) {
-            this.push_data (this.buffer, bytes_read);
+            // FIXME: Remove redundant assingment after we bump our vala dep
+            //        to 0.11.2
+            var data = this.buffer[0:bytes_read];
+            this.push_data (data, bytes_read);
             this.total_length -= bytes_read;
 
             this.run_continue = read_contents.callback;