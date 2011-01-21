GNOME/rygel

commit b0c0aa0db2969d90f33454b4e4b4b17295490278

core: Fix build against vala 0.11.4

src/plugins/tracker/rygel-tracker-years.vala
@@ -38,7 +38,7 @@ public class Rygel.Tracker.Years : MetadataValues {
     }
 
     protected override string? create_title_for_value (string value) {
-        return value.ndup (4);
+        return value.substring (4);
     }
 
     protected override string create_filter (string variable, string value) {

src/rygel/rygel-http-byte-seek.vala
@@ -44,7 +44,7 @@ internal class Rygel.HTTPByteSeek : Rygel.HTTPSeek {
                                                        range);
             }
 
-            range_tokens = range.offset (6).split ("-", 2);
+            range_tokens = range.substring (6).split ("-", 2);
             if (range_tokens[0] == null || range_tokens[1] == null) {
                 throw new HTTPSeekError.INVALID_RANGE (_("Invalid Range '%s'"),
                                                        range);

src/rygel/rygel-http-time-seek.vala
@@ -45,7 +45,7 @@ internal class Rygel.HTTPTimeSeek : Rygel.HTTPSeek {
                                                        range);
             }
 
-            range_tokens = range.offset (4).split ("-", 2);
+            range_tokens = range.substring (4).split ("-", 2);
             if (range_tokens[0] == null || range_tokens[1] == null) {
                 throw new HTTPSeekError.INVALID_RANGE (_("Invalid Range '%s'"),
                                                        range);

src/rygel/rygel-item-creator.vala
@@ -296,8 +296,8 @@ internal class Rygel.ItemCreator: GLib.Object, Rygel.StateMachine {
 
         while (item == null) {
             try {
-                item = yield container.find_object (this.item.id,
-                                                    this.cancellable)
+                item = (yield container.find_object (this.item.id,
+                                                     this.cancellable))
                        as MediaItem;
             } catch (Error error) {
                 warning ("Error from container '%s' on trying to find newly " +

src/rygel/rygel-root-device-factory.vala
@@ -276,7 +276,7 @@ internal class Rygel.RootDeviceFactory {
                               width + "x" +
                               height + "x" +
                               depth + "." + icon_info.file_extension;
-            var local_path = uri.offset (7);
+            var local_path = uri.substring (7);
 
             this.context.host_path (local_path, remote_path);
             icon_node->new_child (null, "url", remote_path);

commit 5cefd204783f79bad15188b2433add1fbe992892

tracker: Fix regression introduced by b0c0aa0

src/plugins/tracker/rygel-tracker-years.vala
@@ -38,7 +38,7 @@ public class Rygel.Tracker.Years : MetadataValues {
     }
 
     protected override string? create_title_for_value (string value) {
-        return value.substring (4);
+        return value.substring (0, 4);
     }
 
     protected override string create_filter (string variable, string value) {