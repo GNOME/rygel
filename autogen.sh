d7874d01bc14ec973068b0ea65dc1215fc49461a
Jens Georg <mail@jensge.org>
2011-02-13 17:22:53 +0100

    build: Option to enable fatal valac warings
    Also make this default when running autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -10,5 +10,5 @@ mkdir -p m4
 # require automak 1.11 for vala support
 export REQUIRED_AUTOMAKE_VERSION=1.11
 gnome-autogen.sh --enable-vala --enable-maintainer-mode --enable-debug \
-                 --enable-test-plugin --enable-mediathek-plugin \
-                 --enable-gst-launch-plugin "$@"
+                 --enable-strict-valac --enable-test-plugin \
+                 --enable-mediathek-plugin --enable-gst-launch-plugin "$@"
