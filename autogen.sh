5318c135a8ebd47e7dc0dcf5ea4d5d9320a3b9e0
Jens Georg <mail@jensge.org>
2011-02-20 00:26:45 +0100

    build: Request build tool versions in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,10 @@ which gnome-autogen.sh || {
 mkdir -p m4
 
 # require automak 1.11 for vala support
-export REQUIRED_AUTOMAKE_VERSION=1.11
+REQUIRED_AUTOMAKE_VERSION=1.11 \
+REQUIRED_AUTOCONF_VERSION=2.64 \
+REQUIRED_LIBTOOL_VERSION=2.2.6 \
+REQUIRED_INTLTOOL_VERSION=0.40.0 \
 gnome-autogen.sh --enable-vala --enable-maintainer-mode --enable-debug \
                  --enable-strict-valac --enable-test-plugin \
                  --enable-mediathek-plugin --enable-gst-launch-plugin "$@"
