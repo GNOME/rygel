GNOME/rygel

commit 905c9e89a1a4262f7337abcd175c32f10ec8ae65

core: Apply Xbox hacks on Philips DLNA TV
Seems Philips TV doesn't add anything to 'user-agent' header so we
are forced to apply the hack on all devices that uses that same DLNA
stack: Allegro Software WebClient.

src/rygel/rygel-xbox-hacks.vala
@@ -29,7 +29,7 @@ internal errordomain Rygel.XBoxHacksError {
 }
 
 internal class Rygel.XBoxHacks : GLib.Object {
-    private static string AGENT = ".*Xbox.*";
+    private static string AGENT = ".*Xbox.*|.*Allegro-Software-WebClient.*";
     private static string DMS = "urn:schemas-upnp-org:device:MediaServer";
     private static string DMS_V1 = DMS + ":1";
     private static string FRIENDLY_NAME_POSTFIX =
@@ -38,7 +38,8 @@ internal class Rygel.XBoxHacks : GLib.Object {
 
     public XBoxHacks.for_action (ServiceAction action) throws XBoxHacksError {
         var agent = action.get_message ().request_headers.get_one ("User-Agent");
-        if (!agent.contains ("Xbox")) {
+        if (!(agent.contains ("Xbox") &&
+              agent.contains ("Allegro-Software-WebClient"))) {
             throw new XBoxHacksError.NA (_("Not Applicable"));
         }
     }

commit 2c429573793d32ddc27837510aea3090c2a026d5

core: Correct condition for applying XBox hacks
This broke in commit 905c9e8.

src/rygel/rygel-xbox-hacks.vala
@@ -39,8 +39,8 @@ internal class Rygel.XBoxHacks : GLib.Object {
     public XBoxHacks.for_action (ServiceAction action) throws XBoxHacksError {
         unowned MessageHeaders headers = action.get_message ().request_headers;
         var agent = headers.get_one ("User-Agent");
-        if (!(agent.contains ("Xbox") &&
-              agent.contains ("Allegro-Software-WebClient"))) {
+        if (!(agent.contains ("Xbox")) &&
+            !(agent.contains ("Allegro-Software-WebClient"))) {
             throw new XBoxHacksError.NA (_("Not Applicable"));
         }
     }