GNOME/rygel

commit 463792da68f7f62d3cbc7b4ac3179d0819652e96

core,plugins: Introducing SearchableContainer
Move MediaContainer.search (and related methods) to separate interface,
SearchableContainer. Not every container needs to be searchable and allowing
search in them only makes searches on top containers slow. Now only the
container that needs to be searchable implement this new interface.

src/plugins/tracker/rygel-tracker-search-container.vala
@@ -116,28 +116,20 @@ public class Rygel.Tracker.SearchContainer : Rygel.MediaContainer {
 
         uint total_matches;
 
-        return yield this.search (expression,
-                                  offset,
-                                  max_count,
-                                  out total_matches,
-                                  cancellable);
+        return yield this.execute_query (expression,
+                                         offset,
+                                         max_count,
+                                         out total_matches,
+                                         cancellable);
     }
 
-    public override async MediaObjects? search (SearchExpression? expression,
-                                                uint              offset,
-                                                uint              max_count,
-                                                out uint          total_matches,
-                                                Cancellable?      cancellable)
-                                                throws GLib.Error {
+    public async MediaObjects? execute_query (SearchExpression? expression,
+                                              uint              offset,
+                                              uint              max_count,
+                                              out uint          total_matches,
+                                              Cancellable?      cancellable)
+                                              throws GLib.Error {
         var results = new MediaObjects ();
-
-        if (expression == null || !(expression is RelationalExpression)) {
-            return yield base.search (expression,
-                                      offset,
-                                      max_count,
-                                      out total_matches,
-                                      cancellable);
-        }
 
         var query = this.create_query (expression as RelationalExpression,
                                        (int) offset,
@@ -164,8 +156,23 @@ public class Rygel.Tracker.SearchContainer : Rygel.MediaContainer {
     public override async MediaObject? find_object (string       id,
                                                     Cancellable? cancellable)
                                                     throws GLib.Error {
-        if (this.is_our_child (id)) {
-            return yield base.find_object (id, cancellable);
+        if (!this.is_our_child (id)) {
+            return null;
+        }
+
+        var expression = new RelationalExpression ();
+        expression.op = SearchCriteriaOp.EQ;
+        expression.operand1 = "@id";
+        expression.operand2 = id;
+
+        uint total_matches;
+        var results = yield this.execute_query (expression,
+                                                0,
+                                                1,
+                                                out total_matches,
+                                                cancellable);
+        if (results.size > 0) {
+            return results[0];
         } else {
             return null;
         }

src/plugins/external/rygel-external-container.vala
@@ -29,7 +29,8 @@ using FreeDesktop;
 /**
  * Represents an external container.
  */
-public class Rygel.External.Container : Rygel.MediaContainer {
+public class Rygel.External.Container : Rygel.MediaContainer,
+                                        Rygel.SearchableContainer {
     public MediaContainerProxy actual_container;
 
     public string host_ip;
@@ -85,19 +86,19 @@ public class Rygel.External.Container : Rygel.MediaContainer {
         return yield this.create_media_objects (children_props, this);
     }
 
-    public override async MediaObjects? search (SearchExpression? expression,
-                                                uint              offset,
-                                                uint              max_count,
-                                                out uint          total_matches,
-                                                Cancellable?      cancellable)
-                                                throws GLib.Error {
+    public async MediaObjects? search (SearchExpression? expression,
+                                       uint              offset,
+                                       uint              max_count,
+                                       out uint          total_matches,
+                                       Cancellable?      cancellable)
+                                       throws GLib.Error {
         if (expression == null || !this.searchable) {
             // Either its wildcard or backend doesn't implement search :(
-            return yield base.search (expression,
-                                      offset,
-                                      max_count,
-                                      out total_matches,
-                                      cancellable);
+            return yield this.simple_search (expression,
+                                             offset,
+                                             max_count,
+                                             out total_matches,
+                                             cancellable);
         }
 
         string[] filter = {};

src/plugins/external/rygel-external-dummy-container.vala
@@ -44,4 +44,10 @@ internal class Rygel.External.DummyContainer : MediaContainer {
                                                       throws Error {
         return new MediaObjects ();
     }
+
+    public override async MediaObject? find_object (string       id,
+                                                    Cancellable? cancellable)
+                                                    throws Error {
+        return null;
+    }
 }

src/plugins/media-export/rygel-media-export-db-container.vala
@@ -22,7 +22,8 @@
 using GUPnP;
 using Gee;
 
-public class Rygel.MediaExport.DBContainer : MediaContainer {
+public class Rygel.MediaExport.DBContainer : MediaContainer,
+                                             SearchableContainer {
     protected MediaCache media_db;
 
     public DBContainer (MediaCache media_db, string id, string title) {
@@ -51,12 +52,12 @@ public class Rygel.MediaExport.DBContainer : MediaContainer {
         return this.media_db.get_children (this, offset, max_count);
     }
 
-    public override async MediaObjects? search (SearchExpression? expression,
-                                                uint              offset,
-                                                uint              max_count,
-                                                out uint          total_matches,
-                                                Cancellable?      cancellable)
-                                                throws GLib.Error {
+    public virtual async MediaObjects? search (SearchExpression? expression,
+                                               uint              offset,
+                                               uint              max_count,
+                                               out uint          total_matches,
+                                               Cancellable?      cancellable)
+                                               throws GLib.Error {
         MediaObjects children = null;
 
         try {
@@ -68,11 +69,11 @@ public class Rygel.MediaExport.DBContainer : MediaContainer {
                                          out total_matches);
         } catch (MediaCacheError error) {
             if (error is MediaCacheError.UNSUPPORTED_SEARCH) {
-                children = yield base.search (expression,
-                                              offset,
-                                              max_count,
-                                              out total_matches,
-                                              cancellable);
+                children = yield this.simple_search (expression,
+                                                     offset,
+                                                     max_count,
+                                                     out total_matches,
+                                                     cancellable);
             } else {
                 throw error;
             }

src/plugins/media-export/rygel-media-export-null-container.vala
@@ -36,4 +36,10 @@ internal class Rygel.NullContainer : MediaContainer {
                                                       throws Error {
         return new MediaObjects ();
     }
+
+    public override async MediaObject? find_object (string id,
+                                                    Cancellable? cancellable)
+                                                    throws Error {
+        return null;
+    }
 }

src/plugins/media-export/rygel-media-export-query-container.vala
@@ -100,7 +100,7 @@ internal class Rygel.MediaExport.QueryContainer : DBContainer {
         }
     }
 
-    public override async MediaObjects? search (SearchExpression? expression,
+    public async override MediaObjects? search (SearchExpression? expression,
                                                 uint              offset,
                                                 uint              max_count,
                                                 out uint          total_matches,

src/plugins/tracker/rygel-tracker-category-all-container.vala
@@ -28,7 +28,8 @@ using Gee;
  * A search container that contains all the items in a category.
  */
 public class Rygel.Tracker.CategoryAllContainer : SearchContainer,
-                                                  WritableContainer {
+                                                  WritableContainer,
+                                                  SearchableContainer {
     /* class-wide constants */
     private const string TRACKER_SERVICE = "org.freedesktop.Tracker1";
     private const string RESOURCES_PATH = "/org/freedesktop/Tracker1/Resources";
@@ -83,6 +84,19 @@ public class Rygel.Tracker.CategoryAllContainer : SearchContainer,
         var urn = this.get_item_info (id, out parent_id);
 
         yield this.remove_entry_from_store (urn);
+    }
+
+    public async MediaObjects? search (SearchExpression? expression,
+                                       uint              offset,
+                                       uint              max_count,
+                                       out uint          total_matches,
+                                       Cancellable?      cancellable)
+                                       throws Error {
+        return yield this.simple_search (expression,
+                                         offset,
+                                         max_count,
+                                         out total_matches,
+                                         cancellable);
     }
 
     private void on_graph_updated (string  class_name,

src/rygel/Makefile.am
@@ -82,6 +82,7 @@ VAPI_SOURCE_FILES = rygel-configuration.vala \
 		    rygel-media-container.vala \
 		    rygel-simple-container.vala \
 		    rygel-writable-container.vala \
+		    rygel-searchable-container.vala \
 		    rygel-media-item.vala \
 		    rygel-audio-item.vala \
 		    rygel-music-item.vala \

commit 35a61a8352237f85d01fff2286bc3e19d282f0a4

tracker: Make SearchContainer searchable again.
This fixes XBox support. It was probably broken by 463792da.

src/plugins/tracker/rygel-tracker-search-container.vala
@@ -29,7 +29,7 @@ using Gee;
 /**
  * A container listing a Tracker search result.
  */
-public class Rygel.Tracker.SearchContainer : Rygel.MediaContainer {
+public class Rygel.Tracker.SearchContainer : SimpleContainer {
     /* class-wide constants */
     private const string TRACKER_SERVICE = "org.freedesktop.Tracker1";
     private const string RESOURCES_PATH = "/org/freedesktop/Tracker1/Resources";
@@ -47,7 +47,7 @@ public class Rygel.Tracker.SearchContainer : Rygel.MediaContainer {
                             ItemFactory        item_factory,
                             QueryTriplets?     triplets = null,
                             ArrayList<string>? filters = null) {
-        base (id, parent, title, 0);
+        base (id, parent, title);
 
         this.item_factory = item_factory;
 