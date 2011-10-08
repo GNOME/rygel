5a488bfb988cb3a54d017c8c09a1a7d27e42e25d
Jens Georg <mail@jensge.org>
2011-10-08 09:59:39 +0200

    build: Add option to always run tests

--- autogen.sh
+++ autogen.sh
@@ -13,5 +13,5 @@ REQUIRED_AUTOCONF_VERSION=2.64 \
 REQUIRED_LIBTOOL_VERSION=2.2.6 \
 REQUIRED_INTLTOOL_VERSION=0.40.0 \
 gnome-autogen.sh --enable-vala --enable-maintainer-mode --enable-debug \
-                 --enable-strict-valac --enable-test-plugin \
+                 --enable-strict-valac --enable-tests --enable-test-plugin \
                  --enable-mediathek-plugin --enable-gst-launch-plugin "$@"
