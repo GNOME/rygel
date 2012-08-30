642bfa1691a32d521ee2dc8c0c094f6350eaeb2b
Jens Georg <mail@jensge.org>
2012-08-30 20:26:43 +0200

    build: Add copyright to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
+# Copyright (C) 2010 Zeeshan Ali (Khattak) <zeeshanak@gnome.org>.
+# Copyright (C) 2011 Jens Georg <mail@jensge.org>.
+# Copyright (C) 2012 Openismus GmbH.
+#
+# Author: Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
+#         Jens Georg <mail@jensge.org>
+#         Murray Cumming <murrayc@openismus.com>
+#
+# This file is part of Rygel.
+#
+# Rygel is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rygel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME git"
     exit 1
