13adf652955b5ea0a96bf4656d067291c313df89
Jens Georg <mail@jensge.org>
2012-08-30 20:22:29 +0200

    examples: Add license and doc for server example

--- examples/standalone-server.c
+++ examples/standalone-server.c
@@ -1,3 +1,36 @@
+/*
+ * Copyright (C) 2012 Openismus GmbH.
+ *
+ * Author: Jens Georg <jensg@openismus.com>
+ *
+ * This file is part of Rygel.
+ *
+ * Rygel is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Rygel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/*
+ * Demo application for librygel-server.
+ *
+ * Creates a stand-aline UPnP server that exposes the contents of the current
+ * folder or the folder passed as argument.
+ *
+ * Usage:
+ *   standalone-server [<folder>]
+ *
+ * The server listens on wlan0 and eth0 by default.
+ */
 
 #include <gio/gio.h>
 #include <rygel-server.h>
