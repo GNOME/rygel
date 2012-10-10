8ec8b7150aa8fa2f24bd86786d23513d07391a2d
Murray Cumming <murrayc@murrayc.com>
2012-10-10 09:29:39 +0200

    build: Do not require valadoc in tarball builds
    * configure.ac: Default --enable-valadoc to no.
    * autogen.sh: Specify --enable-valadoc=yes here, as we already
      enable vala.
    * Makefile.am: Remove --enable-valadoc from
      DISTCHECK_CONFIGURE_FLAGS
    * doc/reference/doc-build.am: Move the ifdef around so that
      the html is always installed.

--- autogen.sh
+++ autogen.sh
@@ -36,7 +36,7 @@ REQUIRED_AUTOMAKE_VERSION=1.11 \
 REQUIRED_AUTOCONF_VERSION=2.64 \
 REQUIRED_LIBTOOL_VERSION=2.2.6 \
 REQUIRED_INTLTOOL_VERSION=0.40.0 \
-bash gnome-autogen.sh --enable-vala --enable-maintainer-mode --enable-debug \
+bash gnome-autogen.sh --enable-vala --enable-valadoc --enable-maintainer-mode --enable-debug \
                  --enable-tests --enable-test-plugin \
                  --enable-mediathek-plugin --enable-gst-launch-plugin \
                  --enable-strict-valac "$@"
