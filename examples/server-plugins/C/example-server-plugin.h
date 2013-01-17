41cccb3575f20c257c54f8af8cb01903eed1f63a
Murray Cumming <murrayc@murrayc.com>
2013-01-17 09:51:44 +0100

    docs: Change C example code license.
    LGPL is not really appropriate for example code.

--- examples/server-plugins/C/example-server-plugin.h
+++ examples/server-plugins/C/example-server-plugin.h
@@ -1,21 +1,8 @@
-/*
- * Copyright (C) 2012 Intel Corporation
+/* Copyright (C) 2012 Intel Corporation
  *
- * This file is part of Rygel.
- *
- * Rygel is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Rygel is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * Permission to use, copy, modify, distribute, and sell this example
+ * for any purpose is hereby granted without fee.
+ * It is provided "as is" without express or implied warranty.
  */
 
 #ifndef __RYGEL_EXAMPLE_SERVER_PLUGIN_H__
