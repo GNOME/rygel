GNOME/rygel

commit 3fae8be115b7b26ee9bae7ff6160f9c1d590f586

server: Validate client supplied createClass value
https://bugzilla.gnome.org/show_bug.cgi?id=702741

src/librygel-server/rygel-object-creator.vala
@@ -145,6 +145,12 @@ internal class Rygel.ObjectCreator: GLib.Object, Rygel.StateMachine {
                                          container.id);
             }
 
+            if (this.didl_object is DIDLLiteContainer &&
+                !this.validate_create_class (container)) {
+                throw new ContentDirectoryError.BAD_METADATA
+                                   (_("upnp:createClass value not supported"));
+            }
+
             yield this.create_object_from_didl (container);
             if (this.object is MediaItem) {
                 yield container.add_item (this.object as MediaItem,
@@ -391,6 +397,23 @@ internal class Rygel.ObjectCreator: GLib.Object, Rygel.StateMachine {
 
         this.action.return ();
         this.completed ();
+    }
+
+    private bool validate_create_class (WritableContainer container) {
+        var didl_cont = this.didl_object as DIDLLiteContainer;
+        var create_classes = didl_cont.get_create_classes ();
+
+        if (create_classes == null) {
+            return true;
+        }
+
+        foreach (var create_class in create_classes) {
+            if (!container.can_create (create_class)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private void handle_error (Error error) {

commit 9b48586af250aef7769a1289daa9992c42023089

server: Try harder to find compatible container
Just grabbing the first container and then validate the create classes
doesn't really work; we search for compatible create classes right away.

Fixes issue introduced with 3fae8be1 if MediaExport has virtual folders
enabled.

src/librygel-server/rygel-object-creator.vala
@@ -287,6 +287,44 @@ internal class Rygel.ObjectCreator: GLib.Object, Rygel.StateMachine {
         }
     }
 
+    private async SearchExpression build_create_class_expression
+                                        (SearchExpression expression) {
+        // Take create-classes into account
+        if (!(this.didl_object is DIDLLiteContainer)) {
+            return expression;
+        }
+
+        var didl_container = this.didl_object as DIDLLiteContainer;
+        var create_classes = didl_container.get_create_classes ();
+        if (create_classes == null) {
+            return expression;
+        }
+
+        var builder = new StringBuilder ("(");
+        foreach (var create_class in create_classes) {
+            builder.append_printf ("(upnp:createClass derivedfrom \"%s\") AND",
+                                   create_class);
+        }
+
+        // remove dangeling AND
+        builder.truncate (builder.len - 3);
+        builder.append (")");
+
+        try {
+            var parser = new SearchCriteriaParser (builder.str);
+            yield parser.run ();
+
+            var rel = new LogicalExpression ();
+            rel.operand1 = expression;
+            rel.op = LogicalOperator.AND;
+            rel.operand2 = parser.expression;
+
+            return rel;
+        } catch (Error error) {
+            assert_not_reached ();
+        }
+    }
+
     /**
      * Find a container that can create items matching the UPnP class of the
      * requested item.
@@ -311,11 +349,16 @@ internal class Rygel.ObjectCreator: GLib.Object, Rygel.StateMachine {
         expression.op = SearchCriteriaOp.DERIVED_FROM;
         expression.operand1 = "upnp:createClass";
 
+        // Add container's create classes to the search expression if there
+        // are some
+        var search_expression = yield this.build_create_class_expression
+                                        (expression);
+
         while (upnp_class != "object") {
             expression.operand2 = upnp_class;
 
             uint total_matches;
-            var result = yield root_container.search (expression,
+            var result = yield root_container.search (search_expression,
                                                       0,
                                                       1,
                                                       out total_matches,