3f934b0f184f5ee45203cb63644b63c4ffab21f1
Jens Georg <mail@jensge.org>
2015-09-26 21:12:40 +0200

    build: Remove gnome-common
    Signed-off-by: Jens Georg <mail@jensge.org>

--- autogen.sh
+++ autogen.sh
@@ -1,33 +1,5 @@
-#! /bin/sh
-
-# Copyright (C) 2010 Zeeshan Ali (Khattak) <zeeshanak@gnome.org>.
-# Copyright (C) 2011 Jens Georg <mail@jensge.org>.
-# Copyright (C) 2012 Openismus GmbH.
-#
-# Author: Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
-#         Jens Georg <mail@jensge.org>
-#         Murray Cumming <murrayc@openismus.com>
-#
-# This file is part of Rygel.
-#
-# Rygel is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Rygel is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME git"
-    exit 1
-}
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
 DEFAULT_ARGS="--enable-vala --enable-maintainer-mode  --enable-debug  --enable-example-plugins --enable-mediathek-plugin --enable-gst-launch-plugin --disable-strict-valac"
 
@@ -41,9 +13,40 @@ elif [ "x$1" = "xrelease" ]; then
     shift
 fi
 
-# require automake 1.11 for vala support
-REQUIRED_AUTOMAKE_VERSION=1.11 \
-REQUIRED_AUTOCONF_VERSION=2.64 \
-REQUIRED_LIBTOOL_VERSION=2.2.6 \
-REQUIRED_INTLTOOL_VERSION=0.40.0 \
-bash gnome-autogen.sh $DEFAULT_ARGS "$@"
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+
+cd $srcdir
+
+(test -f configure.ac) || {
+        echo "*** ERROR: Directory "\`$srcdir\'" does not look like the top-level project directory ***"
+        exit 1
+}
+
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
+        echo "*** If you wish to pass any to it, please specify them on the" >&2
+        echo "*** \`$0\' command line." >&2
+        echo "" >&2
+fi
+
+aclocal --install || exit 1
+glib-gettextize --force --copy || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
+cd $olddir
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure $DEFAULT_ARGS "$@" || exit 1
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
