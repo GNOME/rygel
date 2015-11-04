3da5c72163409489bbce97672f5ce870531f0fc0
Jens Georg <mail@jensge.org>
2015-11-04 18:20:30 +0100

    build: Fix typo in autogen.sh
    Signed-off-by: Jens Georg <mail@jensge.org>

--- autogen.sh
+++ autogen.sh
@@ -31,7 +31,7 @@ PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
 if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
         echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
         echo "*** If you wish to pass any to it, please specify them on the" >&2
-        echo "*** \`$0\' command line." >&2
+        echo "*** \`$0' command line." >&2
         echo "" >&2
 fi
 
