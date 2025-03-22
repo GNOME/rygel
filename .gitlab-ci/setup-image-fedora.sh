a0a79e3887e0de92b61a4c3b1abacbe1a961a56d
Jens Georg <mail@jensge.org>
2025-03-22 18:05:46 +0100

    ci: Update to Fedora 43

--- .gitlab-ci/setup-image-fedora.sh
+++ .gitlab-ci/setup-image-fedora.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 dnf clean all
-pip3 install meson markdown gi-docgen jinja2 Markdown markupsafe pygments toml typogrify furo
+pip3 install meson markdown gi-docgen jinja2 Markdown markupsafe pygments toml typogrify furo pyyaml
